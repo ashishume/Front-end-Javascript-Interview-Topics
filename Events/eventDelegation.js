483741ff51d50a21aeb2fb025eadbcad3de5baea
Ashish Debnath <ashish.debnath@nineleaps.com>
2020-10-16 10:25:58 +0530

    push

--- Events/eventDelegation.js
+++ Events/eventDelegation.js
@@ -1,3 +1,14 @@
-document.querySelector("#category").addEventListener("click", (e) => {
-  console.log(e);
-});
+// document.querySelector("#root").addEventListener("click", (e) => {
+//   // console.log(e);
+
+// });
+
+var array = [1, 2, 3, 4, 5,6,]
+for(var i = 0; i < array.length; i++) {
+  delay(i)
+}
+function delay(i) {
+  setTimeout(() => {
+    console.log(array[i])
+  }, 1000);
+}
