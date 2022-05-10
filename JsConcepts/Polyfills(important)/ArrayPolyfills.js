d04c385eb0ce4d7128044db810170ad94305f6ba
Ashish Debnath <ashishume@gmail.com>
2022-05-10 19:34:46 +0530

    fix: polyfill added

--- JsConcepts/Polyfills(important)/ArrayPolyfills.js
+++ JsConcepts/Polyfills(important)/ArrayPolyfills.js
@@ -15,7 +15,7 @@ Array.prototype.customForEach = function (callback) {
 Array.prototype.customMap = function (callback) {
   let arr = [];
   for (let i = 0; i < this.length; i++) {
-    arr.push(callback([i], i, this));
+    arr.push(callback(this[i], i, this));
   }
   return arr;
 };
