9ddd45a41669915811a5fe78ff248dfea9c0dc95
Ashish Debnath <ashishume@gmail.com>
2022-07-03 23:23:04 +0530

    feat: 5 star rating system added

---
+++ Page(html pages)/5-star-rating/index.js
@@ -0,0 +1,32 @@
+const starContainer = document.querySelector(".star");
+const starItem = document.getElementsByClassName("star-item");
+
+const childNodesCount = starContainer.children.length;
+const childNodes = starContainer.children;
+const highlight = "green";
+const defaultColor = "rgb(210, 210, 20)";
+starContainer.addEventListener("mouseover", (e) => {
+  if (e.target.className === "star-item") {
+    const starItem = e.target;
+    for (let i = 0; i < childNodesCount; i++) {
+      if (childNodes[i].innerText !== starItem.innerText) {
+        childNodes[i].style.background = highlight;
+      } else {
+        childNodes[i].style.background = highlight;
+        break;
+      }
+    }
+    for (let i = childNodesCount - 1; i >= 0; i--) {
+      if (childNodes[i].innerText !== starItem.innerText) {
+        childNodes[i].style.background = defaultColor;
+      } else {
+        break;
+      }
+    }
+  }
+});
+starContainer.addEventListener("mouseleave", (e) => {
+  for (let i = 0; i < childNodesCount; i++) {
+    childNodes[i].style.background = defaultColor;
+  }
+});
