eb4f68f0af1394c6091cea97f50d1949ad9622c7
Ashish Debnath <ashishume@gmail.com>
2022-07-04 23:15:25 +0530

    feat: json stringify added

--- JsConcepts/Polyfills(important)/JsonStrigify.js
+++ JsConcepts/Polyfills(important)/JsonStrigify.js
@@ -5,33 +5,45 @@ const obj = {
     phone: 8557098095,
     official: {
       company: "Soroco",
+      codes: [1, 2, 4, "one", "two"],
     },
   },
 };
 
 console.log("OLD: ", JSON.stringify(obj));
-function stringify(obj) {
-  let result = "";
 
-  result += "{";
+function stringify(data) {
+  if (data === undefined) return undefined;
+  if (data === null) return "null";
+  if (data.toString() === "NaN") return "null";
+  if (data === Infinity) return "null";
+  if (data.constructor === String) return '"' + data.replace(/"/g, '\\"') + '"';
+  if (data.constructor === Number) return String(data);
+  if (data.constructor === Boolean) return data ? "true" : "false";
+  if (data.constructor === Array)
+    return (
+      "[" +
+      data
+        .reduce((acc, v) => {
+          if (v === undefined || v === NaN || v === Infinity) return [...acc, "null"];
+          else return [...acc, stringify(v)];
+        }, [])
+        .join(",") +
+      "]"
+    );
+  if (data.constructor === Object)
+    return (
+      "{" +
+      Object.keys(data)
+        .reduce((acc, k) => {
+          if (data[k] === undefined) return acc;
+          else return [...acc, stringify(k) + ":" + stringify(data[k])];
+        }, [])
+        .join(",") +
+      "}"
+    );
 
-  const lastKey = Object.keys(obj).pop();
-
-  for (let key in obj) {
-    const value = obj[key];
-    result += `"${key}":`;
-
-    if (typeof value === "string") {
-      result += `"${obj[key]}"`;
-    } else if (typeof value === "number") {
-      result += `${obj[key]}`;
-    } else if (typeof value === "object") {
-      result += `${stringify(value)}`;
-    }
-    if (lastKey !== key) result += ",";
-  }
-  result += "}";
-  return result;
+  return "{}";
 }
 
 console.log("NEW :", stringify(obj));
