e1f29f3df6c5ed6d43bf87e4e9a052e0c0f7b2bb
Ashish Debnath <ashishume@gmail.com>
2022-08-01 00:55:07 +0530

    feat: closures statement updated

--- JsConcepts/Closures/closures.js
+++ JsConcepts/Closures/closures.js
@@ -6,6 +6,27 @@ function x() {
   a = 100; //prints a because console.log points to a memory location not the "a" value
   y();
 }
-x();
+x(); //100
 
 // the function along with its lexical scope bundled together forms a closure (above is an example)
+
+/**
+ * Diff between closures and scope
+ * Function within another function is called closure. (3 types local scope outer scope and global scope)
+ * Scope defines what variable we have access. (2 types local scope and global scope)
+ */
+
+var a = 1;
+function func() {
+  return function (b) {
+    return function (c) {
+      return function (d) {
+        /** have access to all the scopes due to closures upto global scope */
+        return a + b + c + d;
+      };
+    };
+  };
+}
+
+const z = func()(3)(3)(3);
+console.log(z); // 10
