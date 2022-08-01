3f076ed2b5e9f1ae4eb2daf6ba48007158b06396
Ashish Debnath <ashishume@gmail.com>
2022-08-01 13:03:35 +0530

    feat: convert func to curried method

--- JsConcepts/Currying-concept/currying.js
+++ JsConcepts/Currying-concept/currying.js
@@ -24,4 +24,23 @@ function sum(a) {
 }
 
 const a = sum(1)(2)(3)(4)(5)(6);
-console.log(a());
+// console.log(a());
+
+/** convert func(a,b,c,d) to func(a)(b)(c)(d) */
+function curry(func) {
+  return function curriedFunc(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return function (...next) {
+        return curriedFunc(...args, ...next);
+      };
+    }
+  };
+}
+
+const sumValue = (a, b, c, d) => a + b + c + d;
+
+const totalSum = curry(sumValue);
+
+// console.log(totalSum(1)(2)(3)(4));
