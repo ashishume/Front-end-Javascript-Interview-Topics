0fa0f3ff72fa6b05acb44a6735743e3890ead45a
Ashish Debnath <ashishume@gmail.com>
2022-08-06 13:23:55 +0530

    feat: added some more module patterns

--- JsConcepts/Module-Pattern/index.js
+++ JsConcepts/Module-Pattern/index.js
@@ -4,11 +4,33 @@ const module = (function () {
   }
   return {
     publicMethod: function () {
-        console.log(privateMethod());  //private method can be accessed here
+      console.log(privateMethod()); //private method can be accessed here
       console.log("public");
     },
   };
 })();
-
 // module.publicMethod(); //prints: public
 // module.privateMethod() //Uncaught TypeError: module.privateMethod is not a function  (private method cannot be accessed here directly)
+
+function ModulePattern() {
+  const name = "Ashish";
+  const age = 24;
+
+  function addSomeAgeValue() {
+    return 2;
+  }
+  function calculateAge() {
+    return age + addSomeAgeValue();
+  }
+  return {
+    returnName: function () {
+      return name;
+    },
+    calculateAge,
+  };
+}
+
+const moduleMethod = new ModulePattern();
+// console.log(moduleMethod.returnName()); // prints "Ashish"
+// console.log(moduleMethod.calculateAge()); //prints "26"
+// console.log(moduleMethod.addSomeAgeValue()); //gives error since its a private method  (Uncaught TypeError: moduleMethod.addSomeAgeValue is not a function)
