1c15e97ac01e0b46e9a84cbd956cbcd5b78a521a
Ashish Debnath <ashishume@gmail.com>
2022-08-08 00:14:32 +0530

    feat: autocomplete search added

---
+++ Page(html pages)/Autocomplete-Search/index.js
@@ -0,0 +1,55 @@
+const searchDiv = document.querySelector("#search-results");
+const list = [
+  "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  "Mens Casual Premium Slim Fit T-Shirts ",
+  "Mens Cotton Jacket",
+  "Mens Casual Slim Fit",
+  "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+  "Solid Gold Petite Micropave ",
+  "White Gold Plated Princess",
+  "Pierced Owl Rose Gold Plated Stainless Steel Double",
+  "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+  "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+];
+
+function onChangeData(event) {
+  const value = event.target.value;
+  /** fetching the filtered data */
+  const filteredArray = myFilter(value);
+  if (filteredArray.length) {
+    /** adding the display block class css */
+    searchDiv.classList.add("show-search");
+    /** create a light document which doesnt effect actual dom tree */
+    const documentFragment = document.createDocumentFragment();
+    filteredArray.forEach((value) => {
+      /** appending each value child to dom tree */
+      const div = document.createElement("div");
+      div.innerHTML = value;
+      documentFragment.appendChild(div);
+    });
+    /** clearing any previous value if present and adding new sugestions to the list */
+    searchDiv.innerHTML = "";
+    searchDiv.appendChild(documentFragment);
+  } else {
+    /** remove the show-search class when no data is there */
+    searchDiv.classList.remove("show-search");
+  }
+}
+
+/** filter the array */
+const myFilter = (value) => {
+  return value !== "" ? list.filter((option) => option.toLowerCase().includes(value)) : [];
+};
+
+/** debounce for some delay */
+const myDebounce = (functions, delay) => {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      functions(...args);
+    }, delay);
+  };
+};
+
+const debounceMethod = myDebounce(onChangeData, 300);
