cb5ae405cbb6d90de222a49546f6407a3759b970
Ashish Debnath <ashishume@gmail.com>
2023-02-27 20:05:22 +0530

    modified:   Flux.js

--- Flux.js
+++ Flux.js
@@ -6,7 +6,7 @@ Actions - Helper methods that facilitate passing data to the Dispatcher
 Dispatcher - Receives actions and broadcasts payloads to registered callbacks
 Stores - Containers for application state & logic that have callbacks registered to the dispatcher
 Controller Views - React Components that grab the state from Stores and pass it down via props to child components. 
-
+dfs
 
 
 */
