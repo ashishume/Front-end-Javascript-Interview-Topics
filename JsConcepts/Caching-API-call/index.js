4e42b446c00ffb7f62ad2ac431135ccf5d2b5601
Ashish Debnath <ashishume@gmail.com>
2023-03-03 00:11:08 +0530

    feat: api caching added

---
+++ JsConcepts/Caching-API-call/index.js
@@ -0,0 +1,31 @@
+let cache = {};
+
+function cacheMethod(url) {
+  //cache the url and check if expiry date is passed or not
+  if (cache[url] && cache[url].expiresAt > Date.now()) {
+    return Promise.resolve(cache[url].data);
+  }
+
+  //if cache is not present then make a new api call
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      //cache duration
+      const cacheDuration = 60 * 1000;
+      const expiresAt = Date.now() + cacheDuration;
+      const data = res.products;
+      //pass the data to the cache
+      cache[url] = { data, expiresAt };
+
+      return data;
+    });
+}
+
+cacheMethod("https://dummyjson.com/products?limit=10").then((d) => {
+  console.log("d==>", d);
+
+  //second call is not made in network tab, only promise is being return
+  cacheMethod("https://dummyjson.com/products?limit=10").then((a) => {
+    console.log("a==>", a);
+  });
+});
