1b642cef10f8a873e10c0a21ba9ae8b5cb2daf06
Ashish Debnath <ashishume@gmail.com>
2023-08-22 22:32:57 +0530

    feat: highlight string

---
+++ JsConcepts/LearnersBucket(youtube channel)/HighlightWord/index.js
@@ -0,0 +1,31 @@
+const str = "Ultimate Javascript / FrontEnd Guide";
+const words = ["End", "Javascript"];
+const div = document.getElementById("root");
+function splitWordsBySpaceOrCapitalLetter(str, words) {
+  const newStr = str.split(" ");
+  return newStr
+    .map((word) => {
+      if (words.includes(word)) {
+        return `<strong>${word}</strong>`;
+      } else {
+        for (let i = 0; i < word.length; i++) {
+          prefix = word.slice(0, i + 1);
+          suffix = word.slice(i + 1);
+
+          if (words.includes(prefix) && words.includes(suffix)) {
+            return `<strong>${prefix + suffix}</strong>`;
+          } else if (words.includes(prefix) && !words.includes(suffix)) {
+            return `<strong>${prefix}</strong>${suffix}`;
+          } else if (!words.includes(prefix) && words.includes(suffix)) {
+            return `${prefix}<strong>${suffix}</strong>`;
+          }
+        }
+      }
+      return word;
+    })
+    .join(" ");
+}
+
+const newStringArr = splitWordsBySpaceOrCapitalLetter(str, words);
+
+div.innerHTML = newStringArr;
