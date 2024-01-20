dff935c205c45860fbc8e125b0b9e001ac517e26
Ashish Debnath <ashishume@gmail.com>
2024-01-21 01:22:49 +0530

    feat: deep object set

--- JsConcepts/deep-object-insert-Lodash-Insert/index.js
+++ JsConcepts/deep-object-insert-Lodash-Insert/index.js
@@ -15,17 +15,23 @@ const obj = {};
 // };
 
 function setObj(obj, path, val) {
-  const pathArr = path.replaceAll("[", ".").replaceAll("]", "").split(".");
+  /* remove all the brackets and  replace them with dot(.) */
+  const pathArr = path.replaceAll("[", ".").replaceAll("]", "").split("."); // ["a","0","b","c"]
   return helper(obj, pathArr, val);
 }
 
 function helper(obj, pathArr, val) {
+  /** get the current value and spread the remaing array values */
   const [curr, ...rest] = pathArr;
   if (rest?.length > 0) {
+    /** if current obj doesnt exist then create one, if number then array else object */
     if (!obj[curr]) {
+      // `{+rest[0]} converts the string to numeric value
       const isNumber = `${+rest[0]}` === rest[0];
       obj[curr] = isNumber ? [] : {};
     }
+
+    /** if current object is already present then, call helper recursively */
     if (typeof obj[curr] === "object") {
       const isNumber = `${+rest[0]}` === rest[0];
       obj[curr] = helper(isNumber ? [] : {}, rest, val);
@@ -35,7 +41,7 @@ function helper(obj, pathArr, val) {
   } else {
     obj[curr] = val;
   }
-  console.log(obj);
+  // console.log(obj);
   return obj;
 }
 console.log(setObj(obj, pathArr, val));
