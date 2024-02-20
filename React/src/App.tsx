1f38efd5784386cad6f8d329fce3ab655436457e
Ashish Debnath <ashishume@gmail.com>
2024-02-21 01:03:21 +0530

    feat: search added for topics

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,14 +1,27 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
 import { routes } from "./Routes/routes";
-import { Fragment } from "react";
+import { Fragment, ReactElement, useState } from "react";
 const App = () => {
+  const [routeData, setRouteData] = useState(
+    routes as { routeName: string; component: ReactElement }[]
+  );
+
+  function handleSearch(e: any) {
+    const searchData = e.target.value;
+    const res = routes.filter(
+      (value: { routeName: string; component: ReactElement }) =>
+        value.routeName.includes(searchData)
+    );
+    setRouteData(res);
+  }
+
   return (
     <div className="container">
       <h1>Interview Concepts</h1>
+      <input placeholder="search topics..." onChange={handleSearch} />
       <div className="routing-container">
-        {routes.map(({ routeName }) => {
-
+        {routeData.map(({ routeName }) => {
           // remove nested routes
           routeName = routeName.includes("/*")
             ? routeName.replaceAll("/*", "/")
