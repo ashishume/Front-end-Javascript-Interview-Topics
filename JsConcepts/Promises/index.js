945d948d203117f962e1485743274a762afaff1e
Ashish Debnath <ashishume@gmail.com>
2024-03-05 23:18:44 +0530

    feat: promises

--- JsConcepts/Promises/index.js
+++ JsConcepts/Promises/index.js
@@ -40,51 +40,51 @@ const fetchGitUsers = () =>
  * doesnt print anything in the then statement,
  * goes into the catch statement
  */
-// Promise.all([fetchGitUsers(), fetchPlaceholder()])
-//   .then((d) => {
-//     console.log("all",d);
-//   })
-//   .catch((e) => {
-//     console.log(e.message);
-//   })
-//   .finally((d) => {});
+Promise.all([fetchGitUsers(), fetchPlaceholder()])
+  .then((d) => {
+    console.log("all",d);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally((d) => {});
 
 /** when all the promises are independent of each other, whether they have failed/completed
  * it prints the status after everything is settled with a status of rejection or fulfillment
  * it prints as an object ==> {status: 'rejected', reason: Promise}
  * ==>{status: 'fulfilled', value: Array(10)}
  */
-// Promise.allSettled([fetchGitUsers(), fetchPlaceholder()])
-//   .then((d) => {
-//     console.log("all settled", d);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+Promise.allSettled([fetchGitUsers(), fetchPlaceholder()])
+  .then((d) => {
+    console.log("all settled", d);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 /** whichever promise finishes first(only one promise) will get returned as the callback,
  * it only prints the resolved ones, ignores the rejected ones.
  * if none of the promises gets resolved then, throws an
  * "AggregateError: All promises were rejected"
  */
-// Promise.any([fetchGitUsers(), fetchPlaceholder()])
-//   .then((d) => {
-//     console.log("any", d);
-//   })
-//   .catch((e) => {
-//     console.log("any error", e);
-//   });
+Promise.any([fetchGitUsers(), fetchPlaceholder()])
+  .then((d) => {
+    console.log("any", d);
+  })
+  .catch((e) => {
+    console.log("any error", e);
+  });
 
 /** whenever any promises gets fulfilled or rejected first (only one promise),
  * then it gets returned as a value if fullfilled, or reason if rejected
  * */
-// Promise.race([fetchGitUsers(), fetchPlaceholder()])
-//   .then((d) => {
-//     console.log("race", d);
-//   })
-//   .catch((e) => {
-//     console.log("race error", e);
-//   });
+Promise.race([fetchGitUsers(), fetchPlaceholder()])
+  .then((d) => {
+    console.log("race", d);
+  })
+  .catch((e) => {
+    console.log("race error", e);
+  });
 
 //Async await, serves the same purpose as promise
 const fetchData = async () => {
