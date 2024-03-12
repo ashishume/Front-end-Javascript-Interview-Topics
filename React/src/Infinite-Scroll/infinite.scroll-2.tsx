e1ad9ddc1371bc360693b4661af904e3943151a7
Ashish Debnath <ashishume@gmail.com>
2024-03-12 23:20:18 +0530

    feat: infite scroll fixed

--- React/src/Infinite-Scroll/infinite.scroll-2.tsx
+++ React/src/Infinite-Scroll/infinite.scroll-2.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const InfiniteScroll = () => {
   const [products, setProducts] = useState<any[]>([]);
   const [page, setPageNumber] = useState<number>(1);
 
-  const fetchNewData = async () => {
-    await setPageNumber((prev) => prev + 1); // TODO: this page number is not updating
-    const response = await fetch(
-      `https://5e217fb26867a0001416f3e8.mockapi.io/employee?page=${page}&limit=10`
-    );
-    const newData = await response.json();
-    await setProducts((prevData) => [...prevData, ...newData]);
-  };
   useEffect(() => {
-    fetchNewData();
+    /** add event listener for scroll */
     window.addEventListener("scroll", handleScroll);
+
+    /** call the method immediately (IIFEs)*/
+    (async function () {
+      const response = await fetch(
+        `https://5e217fb26867a0001416f3e8.mockapi.io/employee?page=${page}&limit=10`
+      );
+      const newData = await response.json();
+      await setProducts((prevData) => [...prevData, ...newData]);
+    })();
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [page]);
 
   function handleScroll() {
-    const distanceFromBottom = Math.floor(
-      document.documentElement.offsetHeight -
-        (window.innerHeight + window.scrollY)
-    );
-    if (distanceFromBottom < 100) {
-      fetchNewData();
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      // User has scrolled to the bottom, make API call
+      setPageNumber((prev) => prev + 1);
     }
   }
 
@@ -36,7 +36,7 @@ const InfiniteScroll = () => {
         products.map((product) => {
           return (
             <div
-              //   key={product.id}
+              key={product.id}
               style={{
                 margin: "50px 0",
                 backgroundColor: "skyblue",
