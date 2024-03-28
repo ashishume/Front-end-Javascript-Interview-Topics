7546429becaf93400d064548ca6e0af9015885bc
Ashish Debnath <ashishume@gmail.com>
2024-03-29 00:23:52 +0530

    feat: optimise api calls

--- React/src/OptimiseMakingApiCalls/index.tsx
+++ React/src/OptimiseMakingApiCalls/index.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 /** create a batch of api calls and make parallel api calls to reduce the loading time */
 const fetchUserIds = () => {
   return new Promise((resolve, reject) => {
+    /**mocking a delayed api call to get the ids of the users */
     setTimeout(() => {
       resolve({
         users: [
@@ -13,28 +14,35 @@ const fetchUserIds = () => {
 };
 
 const OptimiseMakingApiCalls = () => {
+  /** state to store the fetched data */
   const [data, setData] = useState([] as any);
-  const fetchUserData = (id: number): any => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((data) => data.json())
-      .then((data) => {
-        setData((prev: any) => [...prev, data]);
-      });
-  };
 
-  const batchSize = 10;
   useEffect(() => {
+    const batchSize = 10;
     fetchUserIds().then(async (res: any) => {
+      /** fetching the users data in batches and making parallel calls using Promise.all() */
       for (let i = 0; i < res.users.length; i += batchSize) {
+        /** slicing through each batch ids and making concurrent api calls */
         const users = res.users.slice(i, i + batchSize);
+
+        /** promise.all ensures parallel api calls */
         await Promise.all(users.map((val: number) => fetchUserData(val)));
       }
     });
   }, []);
-  console.log(data);
 
+  /** fetch each user data based on id */
+  const fetchUserData = (id: number): any => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((data) => data.json())
+      .then((data) => {
+        /** concatinating each users data into a state */
+        setData((prev: any) => [...prev, data]);
+      });
+  };
   return (
     <div>
+      {/* Print the loaded data */}
       {data?.map((value: any) => {
         return <div key={value.id}>{value.title}</div>;
       })}
