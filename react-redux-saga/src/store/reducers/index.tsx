1671d238612420e6ac92578fec2eb87bcda9f1fc
Ashish Debnath <ashishume@gmail.com>
2024-06-17 14:53:51 +0530

    feat: redux saga removed

--- react-redux-saga/src/store/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-// reducers/index.ts
-import { combineReducers } from "@reduxjs/toolkit";
-import exampleReducer from "../slices/exampleSlice";
-
-const rootReducer = combineReducers({
-  example: exampleReducer,
-});
-
-export default rootReducer;
--- react-redux-saga/src/store/sagas/index.tsx
+++
@@ -1,8 +0,0 @@
-import { all, call } from "redux-saga/effects";
-import exampleSaga from "./exampleSaga";
-
-function* rootSaga() {
-  yield all([call(exampleSaga)]);
-}
-
-export default rootSaga;
