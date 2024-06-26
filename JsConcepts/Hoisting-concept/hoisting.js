d7f92acf7693c758327e2d09ff455b0f7aa55edd
Ashish Debnath <ashishume@gmail.com>
2024-06-26 21:41:02 +0530

    hoisting formating

--- JsConcepts/Hoisting-concept/hoisting.js
+++ JsConcepts/Hoisting-concept/hoisting.js
@@ -5,7 +5,9 @@ var a;
 
 /**
 
-Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of their containing scope during the compilation phase. However, this doesn't mean that the actual declarations are physically moved in the code.
+Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top 
+of their containing scope during the compilation phase. However, this doesn't mean that the actual
+declarations are physically moved in the code.
 
  */
 
