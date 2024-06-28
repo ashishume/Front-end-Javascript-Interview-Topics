2cc82d7036aa14f7759f3ccc5c48cae7fadaca81
Ashish Debnath <ashishume@gmail.com>
2024-06-28 23:27:41 +0530

    feat: stepper completed

--- react-coding-problems/src/Projects/Multi-Stepper/multi-stepper.tsx
+++ react-coding-problems/src/Projects/Multi-Stepper/multi-stepper.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import tick from "./tick.png";
 import "./style.scss";
 import { Button } from "@mui/material";
 const MultiStepper = () => {
   const ref = useRef([] as any);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [progressPercent, setProgressPercent] = useState(0);
   const stepper = [
     {
       stepName: "Customer Info",
@@ -24,12 +26,33 @@ const MultiStepper = () => {
   ];
   const [activeIndex, setActiveIndex] = useState(0);
 
+  /** continue the steps */
   const continueStep = () => {
     if (activeIndex < stepper.length - 1) {
       setActiveIndex((prev) => prev + 1);
     }
   };
 
+  /**
+   * calculate the percent based on elements position from the screen
+   * @param element
+   * @returns
+   */
+  function calculatePercentage(element: HTMLDivElement) {
+    const elementRect = element.getBoundingClientRect();
+    const containerRect = containerRef.current?.getBoundingClientRect();
+
+    /** get elements center position */
+    const elementX = elementRect.left + elementRect.width / 2;
+    const containerLeft = containerRect?.left as number;
+    const containerWidth = containerRect?.width as number;
+
+    // calculate the percent based on screen width for the element's position
+    const percentage = ((elementX - containerLeft) / containerWidth) * 100;
+    return percentage;
+  }
+
+  /** render the steps content */
   const renderContent = () => {
     switch (activeIndex) {
       case 0: {
@@ -161,27 +184,37 @@ const MultiStepper = () => {
   };
 
   useEffect(() => {
-    const currElementBounds = ref.current[activeIndex]?.getBoundingClientRect();
-    console.log(currElementBounds);
+    const currentStepElement = ref.current[activeIndex];
+    setProgressPercent(calculatePercentage(currentStepElement));
   }, [ref.current, activeIndex]);
 
-  
   return (
-    <div className="container">
-      <div className="horizontal-bar"></div>
+    <div className="multi-stepper-container" ref={containerRef}>
+      <div
+        className="horizontal-bar"
+        style={{
+          width: `${progressPercent}%`,
+        }}
+      ></div>
       <div className="flex justify-between">
         {stepper.map(({ stepName, index }) => {
           return (
-            <div
-              className="flex flex-col items-center gap-1 z-[1]"
-              key={index}
-              ref={(element) => (ref.current[index] = element)}
-            >
-              <div className="rounded-full w-[50px] h-[50px] bg-slate-400">
-                {activeIndex === index ? <img src={tick} /> : null}
+            <Fragment key={index}>
+              <div
+                className="flex flex-col items-center gap-1 z-[1]"
+                key={index}
+              >
+                <div
+                  className="rounded-full w-[50px] h-[50px] bg-slate-400"
+                  ref={(element) => (ref.current[index] = element)}
+                >
+                  {activeIndex === index || activeIndex >= index ? (
+                    <img src={tick} />
+                  ) : null}
+                </div>
+                <div>{stepName}</div>
               </div>
-              <div>{stepName}</div>
-            </div>
+            </Fragment>
           );
         })}
       </div>
