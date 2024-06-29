84307e0000784c6564de38217f627e50b7d7aace
Ashish Debnath <ashishume@gmail.com>
2024-06-29 20:54:39 +0530

    feat: memory game requirements added

--- react-coding-problems/src/Projects/Memory-Game/index.tsx
+++ react-coding-problems/src/Projects/Memory-Game/index.tsx
@@ -1,3 +1,16 @@
+/**
+ * Requirements
+- Display a grid of faced-down cards on the screen, with each card representing a different item or image. You can use emojis as the image, a list of emojis has been provided.
+- The grid should consist of an equal number of cards to make pairs.
+- When a player clicks on a card, it should flip over and reveal its image.
+- Allow the player to select two cards at a time.
+- If the two selected cards have the same image, it's a match and they should remain face-up.
+- If the two selected cards have different images and the player
+- Selects other cards, the two selected cards should flip back.
+- Do nothing, the two selected cards should flip back facedown after a short delay.
+- When all pairs have been successfully matched, end the game and display a "Play again" button.
+ */
+
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 export interface Emoji {
