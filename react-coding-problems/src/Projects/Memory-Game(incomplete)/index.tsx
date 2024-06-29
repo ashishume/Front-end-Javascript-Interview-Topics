40f40d584cca57a6122f413a7216ed4b400a809e
Ashish Debnath <ashish.debnath@soroco.com>
2024-06-29 11:26:49 +0530

    feat: memory updates

--- react-coding-problems/src/Projects/Memory-Game(incomplete)/index.tsx
+++ react-coding-problems/src/Projects/Memory-Game(incomplete)/index.tsx
@@ -1,29 +1,27 @@
 const MemoryGame = () => {
   const emojis = [
-    "🐵",
-    "🐶",
-    "🦊",
-    "🐱",
-    "🦁",
-    "🐯",
-    "🐴",
-    "🦄",
-    "🦓",
-    "🦌",
-    "🐮",
-    "🐷",
-    "🐭",
-    "🐹",
-    "🐻",
-    "🐨",
-    "🐼",
-    "🐽",
-    "🐸",
-    "🐰",
-    "🐙",
+    ["🐵", "🐶", "🦊", "🐱"],
+    ["🦁", "🐯", "🐴", "🦄"],
+    ["🐵", "🐶", "🦊", "🐱"],
+    ["🦁", "🐯", "🐴", "🦄"],
   ];
 
-  return <div>Memory game</div>;
+  return (
+    <div className="container">
+      <div>Memory game</div>
+      <div className="border inline-block">
+        {emojis.map((row) => {
+          return (
+            <div className="flex justify-center gap-4">
+              {row.map((col) => {
+                return <div className="border inline-block m-2 p-3">{col}</div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MemoryGame;
