e561f51a0f51cea2257112e81553d7f17c60939a
Ashish Debnath <ashish.debnath@soroco.com>
2024-06-30 12:13:42 +0530

    feat: js concepts as links

--- JsConcepts/server.js
+++ JsConcepts/server.js
@@ -15,6 +15,7 @@ const addRoutes = (baseFolder) => {
       console.error(`Error reading folder: ${baseFolder}`, err);
       return;
     }
+
     folders.forEach((folder) => {
       const folderPath = path.join(baseFolder, folder);
       const indexPathHtml = path.join(folderPath, "index.html");
@@ -37,7 +38,7 @@ const addRoutes = (baseFolder) => {
     app.get("/", (req, res) => {
       const folderLinks = folders
         .map((folder) => {
-          return `<li><a href="/${encodeURIComponent(
+          return `<li class="mb-2"><a class="text-blue-500 hover:text-blue-700" href="/${encodeURIComponent(
             folder
           )}">${folder}</a></li>`;
         })
@@ -46,14 +47,20 @@ const addRoutes = (baseFolder) => {
                 <!DOCTYPE html>
                 <html>
                 <head>
-                    <title>JsConcepts Server</title>
+                    <title>Javascript Concepts</title>
                     <script src="https://cdn.tailwindcss.com"></script>
                 </head>
-                <body>
-                    <h1>JsConcepts Folders</h1>
-                    <ul>
-                        ${folderLinks}
-                    </ul>
+                <body class="bg-gray-100 text-gray-900">
+                    <div class="container mx-auto py-10">
+                        <h1 class="text-3xl font-bold mb-5">Javscript Concepts</h1>
+                        <div class='text-sm'>The following links contains the output in console, head on to the github repo for code details
+                        <a href='https://github.com/ashishume/Front-end-Javascript-Interview-Topics/tree/75bec7c7515331a2df0692f08390324348e3702b/JsConcepts' class='text-blue-600 hover:text-blue-800 font-semibold hover:underline' target='_blank'>(Github repo)</a>
+                       
+                        </div>
+                        <ul class="list-disc pl-5">
+                            ${folderLinks}
+                        </ul>
+                    </div>
                 </body>
                 </html>
             `;
