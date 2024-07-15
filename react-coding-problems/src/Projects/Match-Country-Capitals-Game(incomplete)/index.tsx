df99e2d12450fa2399a8f22a3972ed086b9abd43
Ashish Debnath <ashish.debnath@soroco.com>
2024-07-15 23:14:09 +0530

    feat: match and capital and country game

--- react-coding-problems/src/Projects/Match-Country-Capitals-Game(incomplete)/index.tsx
+++ react-coding-problems/src/Projects/Match-Country-Capitals-Game(incomplete)/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CountryData = {
   India: "New Delhi",
@@ -22,18 +22,85 @@ const CountryData = {
   Spain: "Madrid",
   Indonesia: "Jakarta",
 };
-
+export interface IMatch {
+  isKey: boolean;
+  value: string;
+}
 const MatchCountryCapitals = () => {
-  const [countryData, setCountryData] = useState([]);
-
-
+  const [countryState, setCountryData] = useState<IMatch[]>([]);
+  const [match, setMatch] = useState<IMatch[]>([]);
+  const [isFinished, setFinished] = useState(false);
+  useEffect(() => {
+    showPairs();
+  }, []);
   const showPairs = () => {
+    let tempData: IMatch[] = [];
+    Object.entries(CountryData).map(([key, value]) => {
+      tempData.push({ isKey: true, value: key });
+      tempData.push({ isKey: false, value });
+    });
+    tempData.sort(() => Math.random() - 0.5); // order the array randomly
 
+    setCountryData(tempData);
   };
+
+  useEffect(() => {
+    if (match.length === 2) {
+      const [item1, item2] = match;
+      if (item1.isKey !== item2.isKey) {
+        for (let [key, value] of Object.entries(CountryData)) {
+          if (item1.isKey) {
+            if (key === item1.value && value === item2.value) {
+              return setFinished(true);
+            } else {
+              setMatch([]);
+              setFinished(false);
+            }
+          } else {
+            if (key === item2.value && value === item1.value) {
+              return setFinished(true);
+            } else {
+              setMatch([]);
+              setFinished(false);
+            }
+          }
+        }
+      } else {
+        setMatch([]);
+        setFinished(false);
+        return;
+      }
+    }
+  }, [match]);
+
+  const handleClick = (value: IMatch) => {
+    if (match.length <= 1) {
+      setMatch((prev) => [...prev, value]);
+    }
+  };
+
   return (
-    <div>
-      <h1>Country capitals match game</h1>
-      {/* <div>{showPairs()}</div> */}
+    <div className="container">
+      <h1 className="text-xl font-bold">Country capitals match game</h1>
+      <p className="text-sm">Find the country name and its capital to win.</p>
+      {!isFinished ? (
+        countryState.map((value) => {
+          return (
+            <div
+              key={value.value}
+              onClick={() => handleClick(value)}
+              className={`border rounded-md p-1 m-1 
+              inline-block hover:bg-slate-300 cursor-pointer ${
+                match.includes(value) ? "bg-slate-500 text-white" : null
+              }`}
+            >
+              {value.value}
+            </div>
+          );
+        })
+      ) : (
+        <div>You found the match</div>
+      )}
     </div>
   );
 };
