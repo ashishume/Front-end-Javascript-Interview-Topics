05d540702adbe684bff6dedad556ccf9054c441f
Ashish Debnath <ashishume@gmail.com>
2024-07-15 21:48:42 +0530

    feat: country game updates

--- react-coding-problems/src/Projects/Match-Country-Capitals-Game(incomplete)/index.tsx
+++ react-coding-problems/src/Projects/Match-Country-Capitals-Game(incomplete)/index.tsx
@@ -1,30 +1,41 @@
+import { useState } from "react";
+
 const CountryData = {
-    "India": "New Delhi",
-    "Russia": "Moscow",
-    "China": "Beijing",
-    "USA": "Washington, D.C.",
-    "Japan": "Tokyo",
-    "Germany": "Berlin",
-    "France": "Paris",
-    "Brazil": "Brasília",
-    "Canada": "Ottawa",
-    "Australia": "Canberra",
-    "Italy": "Rome",
-    "South Korea": "Seoul",
-   "United Kingdom": "London",
-    "Mexico": "Mexico City",
-    "South Africa": "Pretoria",
-    "Argentina": "Buenos Aires",
-    "Saudi Arabia": "Riyadh",
-    "Turkey": "Ankara",
-    "Spain": "Madrid",
-    "Indonesia": "Jakarta"
-  };
-  
+  India: "New Delhi",
+  Russia: "Moscow",
+  China: "Beijing",
+  USA: "Washington, D.C.",
+  Japan: "Tokyo",
+  Germany: "Berlin",
+  France: "Paris",
+  Brazil: "Brasília",
+  Canada: "Ottawa",
+  Australia: "Canberra",
+  Italy: "Rome",
+  "South Korea": "Seoul",
+  "United Kingdom": "London",
+  Mexico: "Mexico City",
+  "South Africa": "Pretoria",
+  Argentina: "Buenos Aires",
+  "Saudi Arabia": "Riyadh",
+  Turkey: "Ankara",
+  Spain: "Madrid",
+  Indonesia: "Jakarta",
+};
+
 const MatchCountryCapitals = () => {
-  return <div>
-    <h1>Country capitals match game</h1>
-  </div>;
+  const [countryData, setCountryData] = useState([]);
+
+
+  const showPairs = () => {
+
+  };
+  return (
+    <div>
+      <h1>Country capitals match game</h1>
+      {/* <div>{showPairs()}</div> */}
+    </div>
+  );
 };
 
 export default MatchCountryCapitals;
