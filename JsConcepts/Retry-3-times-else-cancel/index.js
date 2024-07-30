6e16365d17ce80edca5548dff61a4d8bfa3fd34b
Ashish Debnath <ashishume@gmail.com>
2024-07-30 14:56:08 +0530

    feat: retry method

---
+++ JsConcepts/Retry-3-times-else-cancel/index.js
@@ -0,0 +1,39 @@
+/*
+Write a retry "wrapperFn" which takes a function and retryCount.
+It will try calling the function till it exhaust retryCount and return error at end if not completed else if successful returns the result
+*/
+function asyncFn() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      rej("Failed");
+    }, 2000);
+    setTimeout(() => {
+      res("accepted");
+    }, 3000);
+  });
+}
+
+// Implement this function
+async function wrapperFn(callback, retryCount) {
+  let attempts = 0;
+  while (attempts < retryCount) {
+    console.log('called');
+    try {
+      return await callback();
+    } catch (e) {
+      attempts++;
+      if (attempts >= retryCount) {
+        throw new Error(e);
+      }
+    }
+  }
+}
+
+wrapperFn(asyncFn, 3)
+  .then((res) => console.log(res))
+  .catch((error) => console.error("Final Error:", error));
+
+/**
+ *
+ *
+ */
