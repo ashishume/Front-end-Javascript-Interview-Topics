0d9abd8dc09e1bd9e922249f49a5d66cfa62b6da
Ashish Debnath <ashishume@gmail.com>
2024-08-07 23:55:30 +0530

    prpl pattern

---
+++ Design Patterns/PRPL-pattern.js
@@ -0,0 +1,11 @@
+/**
+ * 
+  The PRPL pattern is a web performance optimization strategy advocated by Google, especially 
+  for Progressive Web Apps (PWAs). PRPL stands for:
+
+- Push critical resources for the initial URL route.
+- Render the initial route as soon as possible.
+- Pre-cache remaining routes.
+- Lazy-load and create remaining routes on demand.
+ */
+
