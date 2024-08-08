7dd2154d3ef2623a18571a880457d81459536abf
Ashish Debnath <ashishume@gmail.com>
2024-08-08 23:03:36 +0530

    feat: mutation observer

--- JsConcepts/MutationObserver/index.js
+++ JsConcepts/MutationObserver/index.js
@@ -2,6 +2,8 @@
  * The MutationObserver interface provides the ability to watch for changes being made to the DOM tree.
  * It is designed as a replacement for the older Mutation Events feature, which was part of the DOM3 Events
  * specification.
+ * 
+ * mutation observer is also called DOM Observer
  */
 const container = document.querySelector(".container");
 
