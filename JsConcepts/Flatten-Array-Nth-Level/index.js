ca82f86aaf2d502586ccf881443f57074fdb39d5
Ashish Debnath <ashish.debnath@contentstack.com>
2025-09-10 01:49:44 +0530

    feat: flatten array updates

--- JsConcepts/Flatten-Array-Nth-Level/index.js
+++ JsConcepts/Flatten-Array-Nth-Level/index.js
@@ -4,18 +4,19 @@ const newArr = [].concat(...arr);
 // console.log(newArr);
 
 /** Flatten the array by more than 1 levels */
-function FlattenMultiLevelArray(arr, depth = 1) {
+function FlattenMultiLevelArray(value) {
   let result = [];
-  arr.forEach((value) => {
-    if (Array.isArray(value) && depth > 0) {
-      result.push(...FlattenMultiLevelArray(value, depth - 1));
-    } else result.push(value);
-  });
-
+  for (let i = 0; i < value.length; i++) {
+    if (Array.isArray(value[i])) {
+      result = result.concat(FlattenMultiLevelArray(value[i]));
+    } else {
+      result.push(value[i]);
+    }
+  }
   return result;
 }
 const levelArr = [1, 2, 3, [5, 7, 8, [4, 8, 5, [1, 2]]]];
-const res = FlattenMultiLevelArray(levelArr, 4);  // Infinity to add N no. of flatten
+const res = FlattenMultiLevelArray(levelArr); // Infinity to add N no. of flatten
 //depth is optional the function can work even without depth(just remove the depth, or provide Infinity to the args)
 
 // console.log(res);
