5e79c3dff0b73bf1bf21c753ed4313e79d770069
Ashish Debnath <ashish.debnath@contentstack.com>
2025-09-18 01:57:53 +0530

    feat: movie booking

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const a = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-let res = [];
-for (let i = 0; i < a.length; i++) {
-  let temp = [];
-
-  for (let j = 0; j < a.length; j++) {
-    temp.push(a[i][j]);
-  }
-  res.push(temp);
+function mergeData(sessions) {
+  let map = [];
+  sessions.map(({ user, duration, equipment }) => {
+    if (!map[user]) {
+      map[user] = {
+        duration,
+        equipment,
+      };
+    } else {
+      let arr = [...map[user].equipment, ...equipment];
+      arr.sort();
+      map[user] = {
+        duration: map[user].duration + duration,
+        equipment: arr,
+      };
+    }
+  });
 }
 
-console.log(res);
+console.log(
+  mergeData([
+    { user: 8, duration: 50, equipment: ["bench"] },
+    { user: 7, duration: 150, equipment: ["dumbbell"] },
+    { user: 1, duration: 10, equipment: ["barbell"] },
+    { user: 7, duration: 100, equipment: ["bike", "kettlebell"] },
+    { user: 7, duration: 200, equipment: ["bike"] },
+    { user: 2, duration: 200, equipment: ["treadmill"] },
+    { user: 2, duration: 200, equipment: ["bike"] },
+  ])
+);
--- react-coding-problems/src/Projects/Movie-Booking/index.tsx
+++ react-coding-problems/src/Projects/Movie-Booking/index.tsx
@@ -1,103 +1,131 @@
+import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const MovieBooking = () => {
-  const [seats] = useState([
+  const [seats, setSeats] = useState([
     [
-      { seatNumber: 1, seatType: "premium" },
-      { seatNumber: 2, seatType: "premium" },
-      { seatNumber: 3, seatType: "premium" },
-      { seatNumber: 4, seatType: "premium" },
-      { seatNumber: 5, seatType: "premium" },
-      { seatNumber: 7, seatType: "premium" },
-      { seatNumber: 8, seatType: "premium" },
-      { seatNumber: 9, seatType: "premium" },
-      { seatNumber: 10, seatType: "premium" },
-      { seatNumber: 11, seatType: "premium" },
-      { seatNumber: 12, seatType: "premium" },
-      { seatNumber: 13, seatType: "premium" },
-      { seatNumber: 14, seatType: "premium" },
-      { seatNumber: 15, seatType: "premium" },
-      { seatNumber: 16, seatType: "premium" },
-      { seatNumber: 17, seatType: "premium" },
-      { seatNumber: 18, seatType: "premium" },
-      { seatNumber: 19, seatType: "premium" },
-      { seatNumber: 20, seatType: "premium" },
+      { isBooked: false, seatNumber: 1, seatType: "premium" },
+      { isBooked: false, seatNumber: 2, seatType: "premium" },
+      { isBooked: false, seatNumber: 3, seatType: "premium" },
+      { isBooked: false, seatNumber: 4, seatType: "premium" },
+      { isBooked: false, seatNumber: 5, seatType: "premium" },
+      { isBooked: false, seatNumber: 7, seatType: "premium" },
+      { isBooked: false, seatNumber: 8, seatType: "premium" },
+      { isBooked: false, seatNumber: 9, seatType: "premium" },
+      { isBooked: false, seatNumber: 10, seatType: "premium" },
+      { isBooked: false, seatNumber: 11, seatType: "premium" },
+      { isBooked: false, seatNumber: 12, seatType: "premium" },
+      { isBooked: false, seatNumber: 13, seatType: "premium" },
+      { isBooked: false, seatNumber: 14, seatType: "premium" },
+      { isBooked: false, seatNumber: 15, seatType: "premium" },
+      { isBooked: false, seatNumber: 16, seatType: "premium" },
+      { isBooked: false, seatNumber: 17, seatType: "premium" },
+      { isBooked: false, seatNumber: 18, seatType: "premium" },
+      { isBooked: false, seatNumber: 19, seatType: "premium" },
+      { isBooked: false, seatNumber: 20, seatType: "premium" },
     ],
     [
-      { seatNumber: 21, seatType: "recliner" },
-      { seatNumber: 22, seatType: "recliner" },
-      { seatNumber: 23, seatType: "recliner" },
-      { seatNumber: 24, seatType: "recliner" },
-      { seatNumber: 25, seatType: "recliner" },
-      { seatNumber: 26, seatType: "recliner" },
-      { seatNumber: 27, seatType: "recliner" },
-      { seatNumber: 28, seatType: "recliner" },
-      { seatNumber: 29, seatType: "recliner" },
-      { seatNumber: 30, seatType: "recliner" },
-      { seatNumber: 31, seatType: "recliner" },
-      { seatNumber: 32, seatType: "recliner" },
-      { seatNumber: 33, seatType: "recliner" },
-      { seatNumber: 34, seatType: "recliner" },
-      { seatNumber: 35, seatType: "recliner" },
-      { seatNumber: 36, seatType: "recliner" },
-      { seatNumber: 37, seatType: "recliner" },
+      { isBooked: false, seatNumber: 21, seatType: "recliner" },
+      { isBooked: false, seatNumber: 22, seatType: "recliner" },
+      { isBooked: false, seatNumber: 23, seatType: "recliner" },
+      { isBooked: false, seatNumber: 24, seatType: "recliner" },
+      { isBooked: false, seatNumber: 25, seatType: "recliner" },
+      { isBooked: false, seatNumber: 26, seatType: "recliner" },
+      { isBooked: false, seatNumber: 27, seatType: "recliner" },
+      { isBooked: false, seatNumber: 28, seatType: "recliner" },
+      { isBooked: false, seatNumber: 29, seatType: "recliner" },
+      { isBooked: false, seatNumber: 30, seatType: "recliner" },
+      { isBooked: false, seatNumber: 31, seatType: "recliner" },
+      { isBooked: false, seatNumber: 32, seatType: "recliner" },
+      { isBooked: false, seatNumber: 33, seatType: "recliner" },
+      { isBooked: false, seatNumber: 34, seatType: "recliner" },
+      { isBooked: false, seatNumber: 35, seatType: "recliner" },
+      { isBooked: false, seatNumber: 36, seatType: "recliner" },
+      { isBooked: false, seatNumber: 37, seatType: "recliner" },
     ],
     [
-      { seatNumber: 38, seatType: "gold" },
-      { seatNumber: 39, seatType: "gold" },
-      { seatNumber: 40, seatType: "gold" },
-      { seatNumber: 41, seatType: "gold" },
-      { seatNumber: 42, seatType: "gold" },
-      { seatNumber: 43, seatType: "gold" },
-      { seatNumber: 44, seatType: "gold" },
-      { seatNumber: 45, seatType: "gold" },
-      { seatNumber: 46, seatType: "gold" },
-      { seatNumber: 47, seatType: "gold" },
-      { seatNumber: 48, seatType: "gold" },
-      { seatNumber: 49, seatType: "gold" },
-      { seatNumber: 50, seatType: "gold" },
-      { seatNumber: 51, seatType: "gold" },
-      { seatNumber: 52, seatType: "gold" },
-      { seatNumber: 53, seatType: "gold" },
+      { isBooked: false, seatNumber: 38, seatType: "gold" },
+      { isBooked: false, seatNumber: 39, seatType: "gold" },
+      { isBooked: false, seatNumber: 40, seatType: "gold" },
+      { isBooked: false, seatNumber: 41, seatType: "gold" },
+      { isBooked: false, seatNumber: 42, seatType: "gold" },
+      { isBooked: false, seatNumber: 43, seatType: "gold" },
+      { isBooked: false, seatNumber: 44, seatType: "gold" },
+      { isBooked: false, seatNumber: 45, seatType: "gold" },
+      { isBooked: false, seatNumber: 46, seatType: "gold" },
+      { isBooked: false, seatNumber: 47, seatType: "gold" },
+      { isBooked: false, seatNumber: 48, seatType: "gold" },
+      { isBooked: false, seatNumber: 49, seatType: "gold" },
+      { isBooked: false, seatNumber: 50, seatType: "gold" },
+      { isBooked: false, seatNumber: 51, seatType: "gold" },
+      { isBooked: false, seatNumber: 52, seatType: "gold" },
+      { isBooked: false, seatNumber: 53, seatType: "gold" },
     ],
     [
-      { seatNumber: 54, seatType: "gold" },
-      { seatNumber: 55, seatType: "gold" },
-      { seatNumber: 56, seatType: "gold" },
-      { seatNumber: 57, seatType: "gold" },
-      { seatNumber: 58, seatType: "gold" },
-      { seatNumber: 59, seatType: "gold" },
-      { seatNumber: 60, seatType: "gold" },
-      { seatNumber: 61, seatType: "gold" },
-      { seatNumber: 62, seatType: "gold" },
-      { seatNumber: 63, seatType: "gold" },
+      { isBooked: false, seatNumber: 54, seatType: "gold" },
+      { isBooked: false, seatNumber: 55, seatType: "gold" },
+      { isBooked: false, seatNumber: 56, seatType: "gold" },
+      { isBooked: false, seatNumber: 57, seatType: "gold" },
+      { isBooked: false, seatNumber: 58, seatType: "gold" },
+      { isBooked: false, seatNumber: 59, seatType: "gold" },
+      { isBooked: false, seatNumber: 60, seatType: "gold" },
+      { isBooked: false, seatNumber: 61, seatType: "gold" },
+      { isBooked: false, seatNumber: 62, seatType: "gold" },
+      { isBooked: false, seatNumber: 63, seatType: "gold" },
     ],
     [
-      { seatNumber: 64, seatType: "gold" },
-      { seatNumber: 65, seatType: "gold" },
-      { seatNumber: 66, seatType: "gold" },
-      { seatNumber: 67, seatType: "gold" },
+      { isBooked: false, seatNumber: 64, seatType: "gold" },
+      { isBooked: false, seatNumber: 65, seatType: "gold" },
+      { isBooked: false, seatNumber: 66, seatType: "gold" },
+      { isBooked: false, seatNumber: 67, seatType: "gold" },
     ],
-    [{ seatNumber: 68, seatType: "gold" }],
   ]);
 
   const [selectedSeats, setSelectedSeats] = useState<
     { seatNumber: number; seatType: string }[]
   >([]);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  const priceMap: { [key: string]: number } = {
+    premium: 300,
+    recliner: 200,
+    gold: 100,
+  };
   useEffect(() => {
-    selectedSeats.map((seat) => {
-      if (seat.seatType === "premium") {
-        setTotalPrice((prev) => prev + 300);
-      }
-      if (seat.seatType === "recliner") {
-        setTotalPrice((prev) => prev + 200);
-      }
-      if (seat.seatType === "gold") {
-        setTotalPrice((prev) => prev + 100);
+    setTotalPrice(
+      selectedSeats.reduce((sum, seat) => sum + priceMap[seat.seatType], 0)
+    );
+  }, [selectedSeats]);
+
+  const confirmBooking = () => {
+    setSeats((row) => {
+      return row.map((col) => {
+        return col.map((seat) => {
+          const shouldBook =
+            seat.isBooked ||
+            selectedSeats.some((v) => v.seatNumber === seat.seatNumber);
+
+          return { ...seat, isBooked: shouldBook };
+        });
+      });
+    });
+
+    setTotalPrice(0);
+    setSelectedSeats([]);
+  };
+
+  const handleSeatBooking = (seat: {
+    seatType: string;
+    seatNumber: number;
+  }) => {
+    setSelectedSeats((prev) => {
+      if (prev.some((v) => v.seatNumber === seat.seatNumber)) {
+        return prev.filter((v) => v.seatNumber !== seat.seatNumber);
+      } else {
+        return [...prev, seat];
       }
     });
-  }, [selectedSeats]);
+  };
 
   return (
     <div>
@@ -106,9 +134,9 @@ const MovieBooking = () => {
           <div key={rowIndex} className="flex">
             {row.map((seat) => (
               <div
-                onClick={() => setSelectedSeats((prev) => [...prev, seat])}
+                onClick={() => handleSeatBooking(seat)}
                 key={seat.seatNumber}
-                className={` h-12 w-12 flex justify-center items-center cursor-pointer border border-gray-400 m-1  ${
+                className={` h-12 w-12 flex flex-col justify-center items-center cursor-pointer border border-gray-400 m-1 ${
                   seat.seatType === "premium" ? "bg-yellow-200" : ""
                 } ${seat.seatType === "recliner" ? "bg-green-200" : ""} ${
                   seat.seatType === "gold" ? "bg-red-200" : ""
@@ -121,14 +149,29 @@ const MovieBooking = () => {
                     ? "!bg-blue-500 text-white"
                     : "hover:bg-gray-100"
                 }
+                ${
+                  seat.isBooked === true
+                    ? "!bg-gray-400 pointer-events-none"
+                    : ""
+                }
                 `}
               >
                 {seat.seatNumber}
+                <br />
+                <div className="font-bold">₹ {priceMap[seat.seatType]}</div>
               </div>
             ))}
           </div>
         ))}
-        ₹ {totalPrice}
+
+        <div>
+          <div>
+            <Button variant="contained" onClick={confirmBooking}>
+              Book
+            </Button>
+          </div>
+          <div className="font-bold text-2xl">₹ {totalPrice}</div>
+        </div>
       </div>
     </div>
   );
